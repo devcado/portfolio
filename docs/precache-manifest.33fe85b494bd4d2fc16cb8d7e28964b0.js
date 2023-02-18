0b8fb70c602eb914284aa7d4e4b8087bc456759a
Ricardo Felisbino <ricardo.relativo@outlook.com>
2023-02-18 17:55:05 -0300

    start template

---
+++ docs/precache-manifest.33fe85b494bd4d2fc16cb8d7e28964b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518b4c2050731802dba36071990cdc98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d40783eee093432883ee",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a307ab3d6dddedc91f71",
+    "url": "./static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "d40783eee093432883ee",
+    "url": "./static/js/2.b6e9f796.chunk.js"
+  },
+  {
+    "revision": "02d09a7acaf98bee41ab476ee03abb62",
+    "url": "./static/js/2.b6e9f796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a307ab3d6dddedc91f71",
+    "url": "./static/js/main.1061b777.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
